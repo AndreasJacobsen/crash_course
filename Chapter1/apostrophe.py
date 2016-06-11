c63c339da3284abe2f62b647f7f6d9ada973b23d
vimes90 <andreas1300@gmail.com>
2016-06-12 01:15:18 +0200

    apostroffer

---
+++ Chapter1/apostrophe.py
@@ -0,0 +1,6 @@
+message = "One of Python's strength is its diverse community."
+print(message)
+
+#men bruker vi bare enkle quotes klarer ikke python se hvor setningen ender ('') siden vi skriver Python's <- da slutter den der
+#message = 'One of Python's strength is its diverse community.'
+#print(message)
