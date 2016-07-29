8ac734fc8b78c239dab534bb99c8475f173fd8be
Andreas Jacobsen <andreas1300@gmail.com>
2016-07-29 23:02:20 +1000

    kommet videre

---
+++ Chapter 4/slicedPlayers.py
@@ -0,0 +1,6 @@
+players =['charles','martina','micheal','florence','eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4]) #when we omit a starting point python auto-starts at the start of the list
+print(players[2:]) #when we omit the second value python goes to the end of the list
+print(players[-3:])#printer navnene til de siste tre spillerne
