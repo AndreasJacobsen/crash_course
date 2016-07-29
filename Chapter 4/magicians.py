12753aa4ef40b671e396d7f0cc1aef1eb6666751
Andreas Jacobsen <andreas1300@gmail.com>
2016-07-29 12:13:17 +0200

    kommet videre

---
+++ Chapter 4/magicians.py
@@ -0,0 +1,30 @@
+magicians = ['alice','david', 'caroline']
+
+for magicians_variable in magicians:
+    print(magicians_variable)
+
+dogs_cats = ['muts', 'cats', 'meow', 'woff']
+
+x = 0
+
+#egen løkke for å øve
+for x in range (0, 3):
+    if x ==1:
+        print ('x er',x)
+    else:
+        print ('nå er x', x)
+
+#la oss printe magicians med title, så de får store navn
+
+magicians = ['alice','david', 'caroline']
+
+for magicians_variable in magicians:
+    print(magicians_variable.title() + " is a magician with capital letters in her name! " )
+    print('I can\'t wait to see your next trick ' + magicians_variable.title() + ".\n")
+
+#How many magicians are there?
+i = 0
+print ('Let\'s count magicians!')
+for magicians_variable in magicians:
+    i = i+1
+    print ("Now we have counted", i, " magicians")
